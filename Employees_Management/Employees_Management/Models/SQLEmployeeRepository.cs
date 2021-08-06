547ca25514ea75cfa3480ecf1fa7db442d5752b1
asmaahassnasr <asmaa14has@gmail.com>
2021-08-06 20:41:38 +0200

    Another implementation for IEmployeeRepo from SQL to store and retrieve objects from DB using EF

---
+++ Employees_Management/Employees_Management/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        public Employee Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
