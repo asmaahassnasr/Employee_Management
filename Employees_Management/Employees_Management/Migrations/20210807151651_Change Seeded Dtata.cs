60ad37fbac67a80ad401ca71869600ba5c4b232d
asmaahassnasr <asmaa14has@gmail.com>
2021-08-07 17:17:33 +0200

    Change Seeded Data From AppDbContext ModelBuilder

---
+++ Employees_Management/Employees_Management/Migrations/20210807151651_Change Seeded Dtata.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Employees_Management.Migrations
+{
+    public partial class ChangeSeededDtata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Emial", "Name" },
+                values: new object[] { 2, 2, "emp2@gmail.com", "Emp002" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Emial", "Name" },
+                values: new object[] { 3, 3, "emp3@gmail.com", "Emp003" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- Employees_Management/Employees_Management/Models/AppDbContext.cs
+++ Employees_Management/Employees_Management/Models/AppDbContext.cs
@@ -17,7 +17,9 @@ namespace Employees_Management.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Employee>().HasData(
-                new Employee { Id=1, Name="Emp001", Emial="emp1@gmail.com", Department=Dept.HR}
+                new Employee { Id=1, Name="Emp001", Emial="emp1@gmail.com", Department=Dept.HR},
+                new Employee { Id=2, Name="Emp002", Emial="emp2@gmail.com", Department=Dept.IT},
+                new Employee { Id=3, Name="Emp003", Emial="emp3@gmail.com", Department=Dept.Payroll}
                 );
         }
     }
