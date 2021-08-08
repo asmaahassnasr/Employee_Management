5d735a50a8825805df1ca5977d79d69b7ed25ce7
asmaahassnasr <asmaa14has@gmail.com>
2021-08-08 09:48:25 +0200

    Register new User 1-- Adding RegisterUserViewModel class

---
+++ Employees_Management/Employees_Management/Models/RegisterUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.Models
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        [Compare("Password", ErrorMessage ="Doesnot match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
