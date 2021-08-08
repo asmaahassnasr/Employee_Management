b71dd1a77f46e798d568955941f2b5f392f8db9f
asmaahassnasr <asmaa14has@gmail.com>
2021-08-08 09:49:44 +0200

    Register new User 2-- Adding Controller and register HttpGet Method

---
+++ Employees_Management/Employees_Management/Controllers/AccountController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
