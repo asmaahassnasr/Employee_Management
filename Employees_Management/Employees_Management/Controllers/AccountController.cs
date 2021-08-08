27be16817d23b2ab1f21f9c221a09c8b56341b84
asmaahassnasr <asmaa14has@gmail.com>
2021-08-08 10:48:56 +0200

    Register new User 5-- HttpPost Registeration method

---
+++ Employees_Management/Employees_Management/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employees_Management.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Employees_Management.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> _userManager , SignInManager<IdentityUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+
+                var result = await userManager.CreateAsync(user,model.Password);
+
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user,isPersistent:false);
+                    return RedirectToAction("index","home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+
+            }
+                return  View(model);
+            
+        }
+    }
+}
--- Employees_Management/Employees_Management/ViewModels/RegisterUserViewModel.cs
+++ Employees_Management/Employees_Management/ViewModels/RegisterUserViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Employees_Management.Models
+namespace Employees_Management.ViewModels
 {
     public class RegisterUserViewModel
     {
