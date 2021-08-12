0fd53546412c07d7602172527d0a564552d07b8a
asmaahassnasr <asmaa14has@gmail.com>
2021-08-12 11:04:42 +0200

    Roles: Creating Adminstration controller and creat httpget method

---
+++ Employees_Management/Employees_Management/Controllers/AdminstrationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.Controllers
+{
+    public class AdminstrationController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+
+        public AdminstrationController(RoleManager<IdentityRole> _roleManager)
+        {
+            roleManager = _roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}
