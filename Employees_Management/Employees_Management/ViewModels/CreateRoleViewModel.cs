72c8eda2899c53a1a4dcc218972c3911715f2e69
asmaahassnasr <asmaa14has@gmail.com>
2021-08-12 11:00:53 +0200

    Roles: Creating role with CreateRoleViewModelClass

---
+++ Employees_Management/Employees_Management/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
