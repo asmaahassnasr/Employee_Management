52a91819f1d0cd9dd4aa8d83d1b50958dbe784dc
asmaahassnasr <asmaa14has@gmail.com>
2021-08-12 11:36:43 +0200

    Editing Role Name : EditeRoleViewModel class

---
+++ Employees_Management/Employees_Management/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+        public string Id { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+        //To display All users that belong to a Role
+        public List<string> Users { get; set; }
+    }
+}
