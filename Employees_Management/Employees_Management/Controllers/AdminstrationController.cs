245a1d9c36ec2862cad50360aa89dcb5926e4bdd
asmaahassnasr <asmaa14has@gmail.com>
2021-08-12 11:50:08 +0200

    Editing Role Name : EditRole HttpGet method

--- Employees_Management/Employees_Management/Controllers/AdminstrationController.cs
+++ Employees_Management/Employees_Management/Controllers/AdminstrationController.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 using Employees_Management.ViewModels;
 using System.Linq;
 using System.Threading.Tasks;
+using Employees_Management.Models;
 
 namespace Employees_Management.Controllers
 {
     public class AdminstrationController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdminstrationController(RoleManager<IdentityRole> _roleManager)
+        public AdminstrationController(RoleManager<IdentityRole> _roleManager , UserManager<ApplicationUser> _userManager)
         {
             roleManager = _roleManager;
+            userManager = _userManager;
         }
 
         [HttpGet]
@@ -47,6 +50,29 @@ namespace Employees_Management.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id {id} Not Found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel { Id = role.Id, RoleName = role.Name };
+
+            foreach (var user in userManager.Users)
+            {
+                if( await userManager.IsInRoleAsync(user,role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
         }
     }
 }
