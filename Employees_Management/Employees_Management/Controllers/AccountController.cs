fa06e36a2be78406ed926bab9dbd94bc56ae5182
asmaahassnasr <asmaa14has@gmail.com>
2021-08-12 09:38:58 +0200

    Implementing LogIn Functionality: LogIn get and post methods

--- Employees_Management/Employees_Management/Controllers/AccountController.cs
+++ Employees_Management/Employees_Management/Controllers/AccountController.cs
@@ -49,7 +49,28 @@ namespace Employees_Management.Controllers
                 return  View(model);
             
         }
-
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email,model.Password,model.RememberMe,false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index","home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty,"Invalid Login");
+                }
+            }
+            return View(model);
+        }
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
